9eda5ceacbc9882eb6ce5466bed0bc50254947bf
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-02-19 12:32:20 -0300

    chore: deixando os atributos como publico

--- src/user.ts
+++ src/user.ts
@@ -1,6 +1,15 @@
+class Adress {
+    constructor(
+        public adress: string,
+        public readonly zipCode: string,
+        public number?: number
+    ) {}
+}
+
 class User {
     name: string;
     email: string;
+    adress: Adress[] = [];
     active?: boolean = true;
 
     constructor(name: string, email: string, active: boolean = false) {
@@ -8,7 +17,17 @@ class User {
         this.email = email;
         this.active = active;
     }
+
+    addAdress(newAdress: Adress): void {
+        this.adress.push(newAdress);
+    }
+
+    getAdress(): Adress[] {
+        return this.adress;
+    }
 }
 
+const adress = new Adress("Rua x", "7576-9873", 129);
 const user = new User("Maria", "Maria@gmail.com", true);
+user.addAdress(adress);
 console.log(user);
